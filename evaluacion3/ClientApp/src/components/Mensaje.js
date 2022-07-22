d7b6bea5c30d17d6c0d08f3f3b7de42147ba42cc
Catalina Chavez <catachavez2132@gmail.com>
2022-07-21 21:38:48 -0400

    Agregar archivos de proyecto.

---
+++ evaluacion3/ClientApp/src/components/Mensaje.js
@@ -0,0 +1,163 @@
+﻿//Importar React
+import React, { useEffect, useState } from 'react'
+
+const usuariosPorDefecto = [
+    { nombre: "Pepe Lota", mensaje: "Todo bien", like: true, saludo: false },
+    { nombre: "Tulio Triviño", mensaje: "Aquí les envío un saludo", like: false, saludo: false }
+]
+
+
+const Mensaje = () => {
+    const [usuarios, setUsuarios] = useState(usuariosPorDefecto)
+    const [nombre, setNombre] = useState("")
+    const [mensaje, setMensaje] = useState("")
+    const [like, setLike] = useState(false)
+    const [saludo, setSaludo] = useState(false)
+
+    const handleNombre = (evento) => {
+        setNombre(evento.target.value)
+    }
+
+    const handleMensaje = (evento) => {
+        setMensaje(evento.target.value)
+    }
+
+    const handleLike = (evento) => {
+        setLike(!like)
+    }
+
+    const handleSaludo = (evento) => {
+        setSaludo(!saludo)
+    }
+
+    const handleEnvio = (evento) => {
+        const nuevoUsuario = {
+            nombre: nombre,
+            mensaje: mensaje,
+            like: like,
+            saludo: saludo
+        }
+        let copiaUsuarios = [...usuarios]
+        copiaUsuarios.push(nuevoUsuario)
+        setUsuarios(copiaUsuarios)
+    }
+
+    useEffect(() => {
+        let copiaUsuarios = [...usuarios]
+        const nuevoUsuario = {
+            nombre: "Patana Triviño",
+            mensaje: "Olis",
+            like: true,
+            saludo: true
+        }
+        copiaUsuarios.push(nuevoUsuario)
+        setUsuarios(copiaUsuarios)
+    }, [])
+
+    return (
+        <>
+            <h1>Enviar Mensaje</h1>
+            <div className="row">
+                <div className="col-6">
+                    <label htmlFor="nombre">Nombre</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        value={nombre}
+                        onChange={handleNombre}
+                        name="nombre"
+                        id="nombre"
+                    />
+                </div>
+            </div>
+
+            <div className="row mt-4">
+                <div className="col-6">
+                    <label htmlFor="mensaje">Mensaje</label>
+                    <textarea
+                        className="form-control"
+                        type="text"
+                        value={mensaje}
+                        onChange={handleMensaje}
+                        name="mensaje"
+                        id="mensaje"
+                        rows="3"
+                    />
+                </div>
+            </div>
+
+            <div className="row mt-4">
+                <div className="col-3">
+                    <div className="form-check">
+                        <input
+                            className="form-check-input"
+                            type="checkbox"
+                            value={like}
+                            onChange={handleLike}
+                            name="like"
+                            id="like"
+                        />
+                        <label className="form-check-label" htmlFor="like">
+                            Dar Like 👍
+                        </label>
+                    </div>
+                </div>
+
+                <div className="col-3">
+                    <div className="form-check">
+                        <input
+                            className="form-check-input"
+                            type="checkbox"
+                            value={saludo}
+                            onChange={handleSaludo}
+                            name="saludo"
+                            id="saludo"
+                        />
+                        <label className="form-check-label" htmlFor="saludo">
+                            Enviar Saludo 👋
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row mt-1">
+                <div className="col-12 justify-content-start mt-3">
+                    <button
+                        className="btn btn-primary"
+                        type="button"
+                        onClick={handleEnvio}
+                    >Enviar</button>
+                </div>
+            </div>
+            <hr />
+            <h3>Mensajes Recibidos</h3>
+            <table className="table table-info table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Reacciones</th>
+                        <th scope="col">Mensajes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {usuarios.map((usuario) => {
+                        return (
+                            <tr>
+                                <td>{usuario.nombre}</td>
+                                <td>
+                                    {usuario.like && "👍"}
+                                    {usuario.saludo && "👋"}
+                                    {!usuario.like && !usuario.saludo && "<Ninguno>"}
+                                </td>
+                                <td>{usuario.mensaje}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </>
+
+    )
+}
+
+export default Mensaje
---
+++ evaluacion3/ClientApp/src/components/Portada.js
@@ -0,0 +1,35 @@
+﻿//Importar React
+import React, { useEffect, useState } from 'react'
+
+const Portada = () => {
+    return (
+        <>
+            <div className="card mb-3 text-white bg-info">
+                <div className="row g-0">
+                    <div className="col-md-4">
+                        <img src="https://scontent.fanf1-1.fna.fbcdn.net/v/t39.30808-6/286992043_3204490733121119_1726754837735019077_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=6gn6Yi72Y5sAX8BaeLM&tn=_DDo-ROg6cGjzqJc&_nc_ht=scontent.fanf1-1.fna&oh=00_AT_kZ1vhK0mGzmKPacLO1KNC6uHgiZRUMFRoloY4ipTq7A&oe=62D9CA11" className="img-fluid rounded-start" />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h1 className="card-title">Catalina Andrea Chavez Palma</h1>
+                            <h5 className="card-text">Soy estudiante de la carrera de Analista Programador en la Universidad Tecnologica de Chile estoy cursando mi utlimo semestre de la carrera. Soy una persona que le gusta aprender mucho sobre la tecnologia, es por esto que elegi esta carrera para ejercerla por el resto de mi vida, esta etapa universitaria me ha ensaño muchas cosas, con respecto a la personas, ha enriquecido mi conocimiento y aprendizaje</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            <h4> Habilidades </h4>
+            <div className="col-md-6 mt-3">
+                <ul className="list-group list-group-numbered mt-6">
+                    <li className="list-group-item list-group-item-info">Trabajo en equipo</li>
+                    <li className="list-group-item list-group-item-info">Adaptabilidad</li>
+                    <li className="list-group-item list-group-item-info">Resolución de problemas</li>
+                    <li className="list-group-item list-group-item-info">Trabajo bajo presión</li>
+                    <li className="list-group-item list-group-item-info">Comunicación</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default Portada
